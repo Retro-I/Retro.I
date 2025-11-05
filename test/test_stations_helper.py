ef78251f00645e2539b464341d49826f87d9ce90
Felix Holfelder <felixholfelder1@t-online.de>
2025-11-05 20:27:15 +0100

    added some tests for station_helper

---
+++ test/test_stations_helper.py
@@ -0,0 +1,81 @@
+import unittest
+from test.base_test import BaseTest
+
+from helper.Constants import Constants
+
+constants = Constants()
+
+
+class TestStationsHelper(BaseTest):
+    def setUp(self):
+        super().setUp()
+
+    def test_load_radio_stations(self):
+        actual = self.stations.load_radio_stations()
+        self.assertEqual(26, len(actual))
+
+    # TODO - Enable and fix this with Issue #81
+    @unittest.skip
+    def test_add_station(self):
+        station_to_add = {
+            "id": "id1234",
+            "favorite": False,
+            "name": "Radiosender1234",
+            "logo": "https://google.de/logo.png",
+            "src": "station_url",
+        }
+
+        self.stations.add_station(station_to_add)
+        stations = self.stations.load_radio_stations()
+
+        self.assertIn(station_to_add, stations)
+
+    def test_delete_station(self):
+        index_to_delete = 0
+
+        station = self.stations.load_radio_stations()[index_to_delete]
+
+        self.stations.delete_station(index_to_delete)
+
+        stations = self.stations.load_radio_stations()
+
+        self.assertEqual(25, len(stations))
+        self.assertNotIn(station, stations)
+
+    def test_get_favorite_station(self):
+        station = self.stations.get_favorite_station()
+        self.assertIsNone(station)
+
+        fav_station = self.stations.load_radio_stations()[0]
+        self.stations.set_favorite_station(fav_station)
+
+        station = self.stations.get_favorite_station()
+        self.assertEqual(station, self.stations.load_radio_stations()[0])
+
+    def test_set_favorite_station(self):
+        station = self.stations.load_radio_stations()[0]
+
+        self.stations.set_favorite_station(station)
+
+        stations = self.stations.load_radio_stations()
+
+        self.assertEqual(26, len(stations))
+        self.assertNotIn(station, stations)  # Weil die Station anfanges `favorite=False` hatte
+        self.assertTrue(stations[0]["favorite"])
+        self.assertTrue(1, len([s for s in stations if s["favorite"]]))
+
+    def test_default_station_autoplay_enabled(self):
+        enabled = self.stations.is_default_station_autoplay_enabled()
+        self.assertTrue(enabled)
+
+    def test_toggle_default_station_autoplay(self):
+        enabled = self.stations.is_default_station_autoplay_enabled()
+        self.assertTrue(enabled)
+
+        self.stations.toggle_default_station_autoplay()
+        enabled = self.stations.is_default_station_autoplay_enabled()
+        self.assertFalse(enabled)
+
+        self.stations.toggle_default_station_autoplay()
+        enabled = self.stations.is_default_station_autoplay_enabled()
+        self.assertTrue(enabled)
