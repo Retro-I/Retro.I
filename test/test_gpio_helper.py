abd79f36691f39f47e47b442a1ae23ce488cc3c2
Felix Holfelder <felixholfelder1@t-online.de>
2025-11-07 22:30:16 +0100

    added tests for gpip_helper

---
+++ test/test_gpio_helper.py
@@ -0,0 +1,24 @@
+from test.base_test import BaseTest
+
+from helper.Constants import Constants
+
+constants = Constants()
+
+
+class TestGpioHelper(BaseTest):
+    def setUp(self):
+        super().setUp()
+
+    def test_default_mappings(self):
+        actual = self.gpio_helper.get_mappings()
+        expected = {
+            "ROTARY_VOLUME_UP": "6",
+            "ROTARY_VOLUME_DOWN": "12",
+            "ROTARY_VOLUME_PRESS": "13",
+            "ROTARY_BASS_UP": "4",
+            "ROTARY_BASS_DOWN": "14",
+            "ROTARY_PITCH_UP": "11",
+            "ROTARY_PITCH_DOWN": "8",
+            "START_PARTY_MODE_BUTTON": "21",
+        }
+        self.assertCountEqual(actual, expected)
