155dae4b68d00db079b15ee89ab35f7c252174fd
Felix Holfelder <felixholfelder1@t-online.de>
2025-11-08 15:16:37 +0100

    added tests for audio_effects

--- test/test_audio_effects.py
+++ test/test_audio_effects.py
@@ -1,7 +1,9 @@
 import unittest
+from test.base_test import BaseTest
 
 
 # TODO - fix and enable this with Issue #79
 @unittest.skip
-class TestAudioEffects(unittest.TestCase):
-    pass
+class TestAudioEffects(BaseTest):
+    def setUp(self):
+        super().setUp()
