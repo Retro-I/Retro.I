ddc9f2cf5e51e465d80768ad24fbfec8ac1e686d
Felix Holfelder <felixholfelder1@t-online.de>
2025-11-13 18:57:57 +0100

    WIP - test with title row

--- components/view/Taskbar.py
+++ components/view/Taskbar.py
@@ -65,12 +65,12 @@ class Taskbar(ft.AppBar):
                     content=ft.Row([self.ico_volume, self.txt_volume]),
                     on_click=lambda e: self.volume_dialog.open_dialog(),
                 ),
-                ft.VerticalDivider(),
+                ft.VerticalDivider(color=ft.colors.ON_SURFACE),
                 ft.Container(
                     content=ft.Row([self.ico_bass, self.txt_bass]),
                     on_click=lambda e: self.bass_dialog.open_dialog(),
                 ),
-                ft.VerticalDivider(),
+                ft.VerticalDivider(color=ft.colors.ON_SURFACE),
                 ft.Container(
                     content=ft.Row([self.ico_pitch, self.txt_pitch]),
                     on_click=lambda e: self.bass_dialog.open_dialog(),
