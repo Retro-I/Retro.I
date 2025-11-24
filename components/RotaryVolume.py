4246b81be552fbbe302f8e82e02ed2d91c60a8c4
Felix Holfelder <felixholfelder1@t-online.de>
2025-11-24 19:41:51 +0100

    cleaned RotaryVolume - less parameters

--- components/RotaryVolume.py
+++ components/RotaryVolume.py
@@ -17,45 +17,49 @@ class RotaryVolume:
     VOLUME_MUTE_PIN = gpio_helper.rotary_volume_press()
 
     def __init__(self, on_taskbar_update, on_strip_toggle_mute, on_strip_update_sound):
+        self.on_taskbar_update = on_taskbar_update
+        self.on_strip_toggle_mute = on_strip_toggle_mute
+        self.on_strip_update_sound = on_strip_update_sound
+
         rotary = pyky040.Encoder(
             CLK=self.VOLUME_UP_PIN, DT=self.VOLUME_DOWN_PIN, SW=self.VOLUME_MUTE_PIN
         )
         rotary.setup(
             step=self.VOLUME_STEP,
-            inc_callback=lambda e: self.inc_sound(on_taskbar_update, on_strip_update_sound),
-            dec_callback=lambda e: self.dec_sound(on_taskbar_update, on_strip_update_sound),
-            sw_callback=lambda: self.toggle_mute(on_taskbar_update, on_strip_toggle_mute),
+            inc_callback=lambda e: self.inc_sound(),
+            dec_callback=lambda e: self.dec_sound(),
+            sw_callback=lambda: self.toggle_mute(),
         )
         rotary_thread = threading.Thread(target=rotary.watch)
         rotary_thread.start()
 
-    def inc_sound(self, on_taskbar_update, on_strip_update_sound):
+    def inc_sound(self):
         if self.COUNTER % 2 == 0:
             value = audio_helper.get_volume() + self.VOLUME_STEP
             if 0 <= value <= 100:
-                self.update(value, on_taskbar_update, on_strip_update_sound)
+                self.update(value)
 
             if value > 100:
-                self.update(100, on_taskbar_update, on_strip_update_sound)
+                self.update(100)
         self.COUNTER += 1
 
-    def dec_sound(self, on_taskbar_update, on_strip_update_sound):
+    def dec_sound(self):
         if self.COUNTER % 2 == 0:
             value = audio_helper.get_volume() - self.VOLUME_STEP
             if 0 <= value <= 100:
-                self.update(value, on_taskbar_update, on_strip_update_sound)
+                self.update(value)
 
             if value < 0:
-                self.update(0, on_taskbar_update, on_strip_update_sound)
+                self.update(0)
         self.COUNTER -= 1
 
-    def toggle_mute(self, on_taskbar_update, on_strip_toggle_mute):
+    def toggle_mute(self):
         is_mute = audio_helper.toggle_mute()
-        on_strip_toggle_mute(is_mute)
-        on_taskbar_update()
+        self.on_strip_toggle_mute(is_mute)
+        self.on_taskbar_update()
 
-    def update(self, value, on_taskbar_update, on_strip_update_sound):
+    def update(self, value):
         if not audio_helper.is_mute():
             audio_helper.set_volume(value)
-            on_strip_update_sound(value)
-            on_taskbar_update()
+            self.on_strip_update_sound(value)
+            self.on_taskbar_update()
