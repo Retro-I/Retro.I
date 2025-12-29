8f31e6a4f9238b7d96bb02b645db3bbb4f117769
Felix Holfelder <felixholfelder1@t-online.de>
2025-12-29 16:35:30 +0100

    added led animation for treble steps

--- helper/Strip.py
+++ helper/Strip.py
@@ -80,32 +80,23 @@ class Strip:
             self.pixels.show()
 
     def update_bass_strip(self, value):
-        def _get_middle_of_strip():
-            return settings_helper.get_led_length() // 2
-
-        def _set_middle():
-            middle = _get_middle_of_strip()
-
-            self.pixels[middle - 1] = WHITE
-            self.pixels[middle] = WHITE
-            self.pixels[middle + 1] = WHITE
-
-        def _get_pixels_for_step() -> int:
-            return settings_helper.get_led_length() // bass_steps_helper.get_steps_count()
-
         def _set_step():
             if value == 0:
                 return
 
-            pixels_for_step = _get_pixels_for_step()
+            pixels_for_step = self._get_pixels_for_step()
 
             pixels_to_draw = pixels_for_step * value
 
             if value < 0:
-                for i in range(_get_middle_of_strip() + pixels_to_draw, _get_middle_of_strip()):
+                for i in range(
+                    self._get_middle_of_strip() + pixels_to_draw, self._get_middle_of_strip()
+                ):
                     self.pixels[i] = GREEN
             else:
-                for i in range(_get_middle_of_strip(), _get_middle_of_strip() + pixels_to_draw):
+                for i in range(
+                    self._get_middle_of_strip(), self._get_middle_of_strip() + pixels_to_draw
+                ):
                     self.pixels[i] = GREEN
 
         self.sound_mode_active = False
@@ -113,9 +104,51 @@ class Strip:
 
         self.disable(save=False)
         _set_step()
-        _set_middle()
+        self._set_middle()
 
         self.pixels.show()
+
+    def update_treble_strip(self, value):
+        def _set_step():
+            if value == 0:
+                return
+
+            pixels_for_step = self._get_pixels_for_step()
+
+            pixels_to_draw = pixels_for_step * value
+
+            if value < 0:
+                for i in range(
+                    self._get_middle_of_strip() + pixels_to_draw, self._get_middle_of_strip()
+                ):
+                    self.pixels[i] = GREEN
+            else:
+                for i in range(
+                    self._get_middle_of_strip(), self._get_middle_of_strip() + pixels_to_draw
+                ):
+                    self.pixels[i] = GREEN
+
+        self.sound_mode_active = False
+        self.wait_proc.set_wait()
+
+        self.disable(save=False)
+        _set_step()
+        self._set_middle()
+
+        self.pixels.show()
+
+    def _get_middle_of_strip(self) -> int:
+        return settings_helper.get_led_length() // 2
+
+    def _set_middle(self):
+        middle = self._get_middle_of_strip()
+
+        self.pixels[middle - 1] = WHITE
+        self.pixels[middle] = WHITE
+        self.pixels[middle + 1] = WHITE
+
+    def _get_pixels_for_step(self) -> int:
+        return settings_helper.get_led_length() // bass_steps_helper.get_steps_count()
 
     def update_strip(self, color):
         self.sound_mode_active = True
