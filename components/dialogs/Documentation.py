34d00147755ce792ac5cd18bf5132750463281de
Felix Holfelder <felixholfelder1@t-online.de>
2026-01-09 15:42:06 +0100

    added qrcode for documentation for SettingInfoDialog

---
+++ components/dialogs/Documentation.py
@@ -0,0 +1,28 @@
+import flet as ft
+
+from helper import QrCodeHelper
+from helper.RevisionHelper import RevisionHelper
+from helper.SystemHelper import SystemHelper
+
+system_helper = SystemHelper()
+revision_helper = RevisionHelper()
+
+
+class Documentation(ft.ListView):
+    def __init__(self):
+        super().__init__()
+
+        self.expand = True
+        self.controls = [
+            ft.Text(
+                spans=[
+                    ft.TextSpan("Die Dokumenatation zum Projekt findest du unter "),
+                    ft.TextSpan(
+                        "https://docs.retroi.de", style=ft.TextStyle(weight=ft.FontWeight.BOLD)
+                    ),
+                ],
+                size=20,
+            ),
+            ft.Divider(),
+            ft.Image(src_base64=QrCodeHelper.str_to_qr_code("https://docs.retroi.de")),
+        ]
