be680310373efcced9cc6508d4321bd602453e5e
Felix Holfelder <felixholfelder1@t-online.de>
2026-01-17 11:37:47 +0100

    Revert "fixed buggy Strip class"
    This reverts commit b9f810fccf54a7f83baade0d433b0767adf9a910.
    
    # Conflicts:
    #	helper/Strip.py

--- helper/Strip.py
+++ helper/Strip.py
@@ -20,23 +20,19 @@ audio_helper = Audio()
 
 
 class Strip:
+    is_active = settings_helper.is_strip_active()
+    sound_mode_active = False
+    curr_color = GREEN
+
+    pixel_pin = board.D10
+    pixel_num = 38
+
+    pixels = neopixel.NeoPixel(pixel_pin, pixel_num, brightness=0)
+    animation = Pulse(pixels, min_intensity=0.1, speed=0.1, period=5, color=BLACK)
+
     def __init__(self):
-        self.is_active = settings_helper.is_strip_active()
-        self.sound_mode_active = False
-        self.curr_color = GREEN
-
-        self.pixel_pin = board.D10
-        self.pixel_num = 38
-
-        self.pixels = neopixel.NeoPixel(
-            self.pixel_pin, self.pixel_num, brightness=0
-        )
-        self.animation = Pulse(
-            self.pixels, min_intensity=0.1, speed=0.1, period=5, color=BLACK
-        )
-
         if settings_helper.is_strip_active():
-            self.pixels.fill(self.curr_color)
+            self.pixels.fill(GREEN)
 
         self.pixels.brightness = settings_helper.get_curr_brightness() / 100
         self.pixels.show()
@@ -60,9 +56,7 @@ class Strip:
         self.sound_mode_active = False
         self.wait_proc.set_wait()
 
-        amount_pixels = math.floor(
-            settings_helper.get_led_length() * (value / 100)
-        )
+        amount_pixels = math.floor(settings_helper.get_led_length() * (value / 100))
         self.pixels.fill(BLACK)
         if amount_pixels == 0 and value > 0:
             self.pixels[0] = GREEN
@@ -94,14 +88,12 @@ class Strip:
 
             if value < 0:
                 for i in range(
-                    self._get_middle_of_strip() + pixels_to_draw,
-                    self._get_middle_of_strip(),
+                    self._get_middle_of_strip() + pixels_to_draw, self._get_middle_of_strip()
                 ):
                     self.pixels[i] = GREEN
             else:
                 for i in range(
-                    self._get_middle_of_strip(),
-                    self._get_middle_of_strip() + pixels_to_draw,
+                    self._get_middle_of_strip(), self._get_middle_of_strip() + pixels_to_draw
                 ):
                     self.pixels[i] = GREEN
 
@@ -125,14 +117,12 @@ class Strip:
 
             if value < 0:
                 for i in range(
-                    self._get_middle_of_strip() + pixels_to_draw,
-                    self._get_middle_of_strip(),
+                    self._get_middle_of_strip() + pixels_to_draw, self._get_middle_of_strip()
                 ):
                     self.pixels[i] = GREEN
             else:
                 for i in range(
-                    self._get_middle_of_strip(),
-                    self._get_middle_of_strip() + pixels_to_draw,
+                    self._get_middle_of_strip(), self._get_middle_of_strip() + pixels_to_draw
                 ):
                     self.pixels[i] = GREEN
 
@@ -156,10 +146,7 @@ class Strip:
         self.pixels[middle + 1] = WHITE
 
     def _get_pixels_for_step(self) -> int:
-        return (
-            settings_helper.get_led_length()
-            // bass_steps_helper.get_steps_count()
-        )
+        return settings_helper.get_led_length() // bass_steps_helper.get_steps_count()
 
     def update_strip(self, color):
         self.sound_mode_active = True
@@ -172,24 +159,20 @@ class Strip:
 
     def toggle_strip(self):
         if settings_helper.is_strip_active():
+            self.animation.freeze()
             self.change_brightness(0, save=False)
             self.is_active = False
             settings_helper.update_settings(is_active=False)
         else:
-            self.change_brightness(
-                settings_helper.get_curr_brightness(), save=False
-            )
-            self.pixels.fill(self.curr_color)
+            self.change_brightness(settings_helper.get_curr_brightness(), save=False)
+            self.animation.resume()
             self.pixels.show()
             self.is_active = True
             settings_helper.update_settings(is_active=True)
 
     def change_brightness(self, value, save=True):
         self.pixels.brightness = value / 100
-        self.pixels.fill(self.curr_color)
-        if self.is_active:
-            self.pixels.show()
-
+        self.pixels.show()
         if save:
             settings_helper.update_settings(
                 settings_helper.is_strip_active(),
