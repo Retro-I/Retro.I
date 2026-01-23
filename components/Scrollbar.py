f29c4f1090b67454cf69d21644d2e910b5d22f1a
Felix Holfelder <felixholfelder1@t-online.de>
2026-01-23 17:02:50 +0100

    fixed scrollbar spaces

--- components/Scrollbar.py
+++ components/Scrollbar.py
@@ -8,16 +8,15 @@ SCROLLBAR_SPACE = 70
 
 
 def with_scrollbar_space(content: ft.Control) -> ft.Control:
-    space = SCROLLBAR_SPACE
-    if not scrollbar_settings_helper.is_scrollbar_enabled():
-        space = 0
-
     p = content.padding
-
     if isinstance(p, int):
         p = ft.padding.Padding(p, p, p, p)
     elif p is None:
         p = ft.padding.Padding(0, 0, 0, 0)
+
+    space = SCROLLBAR_SPACE + p.right
+    if not scrollbar_settings_helper.is_scrollbar_enabled():
+        space = p.right
 
     content.padding = ft.padding.Padding(
         left=p.left, top=p.top, right=space, bottom=p.bottom
