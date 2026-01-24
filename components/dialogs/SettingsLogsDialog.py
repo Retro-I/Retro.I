0d1721c2dbd64eb49e9ba9e6a80ddc08c2c77a99
Felix Holfelder <felixholfelder1@t-online.de>
2026-01-24 15:56:59 +0100

    added dialog to display logs

---
+++ components/dialogs/SettingsLogsDialog.py
@@ -0,0 +1,56 @@
+import threading
+import time
+
+import flet as ft
+
+from helper.LogsHelper import LogsHelper
+from helper.Strip import Strip
+
+strip = Strip()
+logs_helper = LogsHelper()
+
+
+class SettingsLogsDialog(ft.AlertDialog):
+    def __init__(self):
+        super().__init__()
+
+        self.get_logs = False
+        self.logs_text_field = ft.TextField(
+            multiline=True,
+            read_only=True,
+            expand=True,
+            clip_behavior=ft.ClipBehavior.ANTI_ALIAS,
+            text_style=ft.TextStyle(
+                font_family="monospace",
+                size=12,
+            ),
+        )
+
+        self.title = ft.Text("Logs")
+        self.on_dismiss = lambda e: self.close_dialog()
+        self.content = ft.Container(
+            expand=True,
+            content=self.logs_text_field,
+        )
+        self.actions = [
+            ft.FilledButton("Abbrechen", on_click=lambda e: self.close_dialog())
+        ]
+
+    def update_logs(self):
+        while self.get_logs:
+            self.logs_text_field.value = logs_helper.get_logs()
+            self.logs_text_field.update()
+            time.sleep(0.5)
+
+    def open_dialog(self):
+        self.get_logs = True
+        logs_thread = threading.Thread(target=self.update_logs)
+        logs_thread.start()
+
+        self.open = True
+        self.update()
+
+    def close_dialog(self):
+        self.get_logs = False
+        self.open = False
+        self.update()
