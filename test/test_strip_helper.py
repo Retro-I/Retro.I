74b8ff5551afea22de086745bda2765be7926137
Felix Holfelder <felixholfelder1@t-online.de>
2026-02-07 14:35:41 +0100

    added settings attributes for static colors

--- test/test_strip_helper.py
+++ test/test_strip_helper.py
@@ -28,6 +28,13 @@ class TestStripSettingsHelper(BaseTest):
         actual = self.strip_settings_helper.get_curr_brightness()
         self.assertEqual(actual, 100)
 
+    def test_is_static_color(self):
+        self.assertFalse(self.strip_settings_helper.is_static_color())
+
+    def test_get_static_color(self):
+        actual = self.strip_settings_helper.get_static_color()
+        self.assertEqual(actual, "#6A540C")
+
     def test_update_curr_brightness(self):
         actual = self.strip_settings_helper.get_curr_brightness()
         self.assertEqual(actual, 100)
@@ -36,11 +43,45 @@ class TestStripSettingsHelper(BaseTest):
         actual = self.strip_settings_helper.get_curr_brightness()
         self.assertEqual(actual, 12.3)
 
+    def test_update_is_static_color(self):
+        self.strip_settings_helper.update_settings(is_static_color=True)
+        self.assertTrue(self.strip_settings_helper.is_static_color())
+
+        self.strip_settings_helper.update_settings(is_static_color=False)
+        self.assertFalse(self.strip_settings_helper.is_static_color())
+
+    def test_update_static_color(self):
+        self.strip_settings_helper.update_settings(static_color="#000000")
+        actual = self.strip_settings_helper.get_static_color()
+        self.assertEqual(actual, "#000000")
+
+    def test_update_static_color_letter(self):
+        self.strip_settings_helper.update_settings(static_color="#FFFFFF")
+        actual = self.strip_settings_helper.get_static_color()
+        self.assertEqual(actual, "#FFFFFF")
+
+    def test_update_static_color_lower_case(self):
+        self.strip_settings_helper.update_settings(static_color="#ddeeff")
+        actual = self.strip_settings_helper.get_static_color()
+        self.assertEqual(actual, "#DDEEFF")
+
+    def test_update_static_color_not_valid(self):
+        self.strip_settings_helper.update_settings(static_color="#XXYYZZ")
+        actual = self.strip_settings_helper.get_static_color()
+        self.assertEqual(actual, "#6A540C")
+
+    def test_update_static_color_invalid_length(self):
+        self.strip_settings_helper.update_settings(static_color="#ABCDEFABCDEF")
+        actual = self.strip_settings_helper.get_static_color()
+        self.assertEqual(actual, "#6A540C")
+
     def test_get_complete_settings(self):
         actual = self.strip_settings_helper.get_strip_settings()
         expected = {
             "isStripEnabled": True,
             "brightness": 100.0,
             "amountLeds": 38,
+            "isStaticColor": False,
+            "staticColor": "#6A540C",
         }
         self.assertCountEqual(actual, expected)
