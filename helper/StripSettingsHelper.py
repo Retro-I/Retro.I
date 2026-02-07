77068312591c918094980afa9588d3e913bd51c3
Felix Holfelder <felixholfelder1@t-online.de>
2026-02-07 14:35:41 +0100

    added settings attributes for static colors

--- helper/StripSettingsHelper.py
+++ helper/StripSettingsHelper.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 from helper.Constants import Constants
 from helper.SettingsSyncHelper import SettingsSyncHelper
@@ -23,6 +24,14 @@ class StripSettingsHelper:
         settings = self.get_strip_settings()
         return settings["brightness"]
 
+    def is_static_color(self) -> bool:
+        settings = self.get_strip_settings()
+        return settings["isStaticColor"]
+
+    def get_static_color(self) -> str:
+        settings = self.get_strip_settings()
+        return settings["staticColor"].upper()
+
     def get_strip_settings(self):
         def _get_data():
             with open(self.STRIP_SETTINGS_PATH) as file:
@@ -39,7 +48,9 @@ class StripSettingsHelper:
         self,
         is_active: bool | None = None,
         brightness: float | None = None,
-        length: int = None,
+        length: int | None = None,
+        is_static_color: bool | None = None,
+        static_color: str | None = None,
     ):
         _is_active = (
             is_active if is_active is not None else self.is_strip_active()
@@ -48,11 +59,27 @@ class StripSettingsHelper:
             brightness if brightness is not None else self.get_curr_brightness()
         )
         _length = length if length is not None else self.get_led_length()
+        _is_static_color = (
+            is_static_color
+            if is_static_color is not None
+            else self.is_static_color()
+        )
+
+        regex = "^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$"
+        p = re.compile(regex)
+        if static_color is None:
+            _static_color = self.get_static_color()
+        elif re.search(p, static_color):
+            _static_color = static_color
+        else:
+            _static_color = self.get_static_color()
 
         data = self.get_strip_settings()
         data["isStripEnabled"] = _is_active
         data["brightness"] = _brightness
         data["amountLeds"] = _length
+        data["isStaticColor"] = _is_static_color
+        data["staticColor"] = _static_color
 
         with open(self.STRIP_SETTINGS_PATH, "r+") as file:
             file.seek(0)
