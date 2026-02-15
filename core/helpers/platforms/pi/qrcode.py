b1252611546a7943a465f7a95c11a5ada09a9835
Felix Holfelder <felixholfelder1@t-online.de>
2026-02-15 12:45:07 +0100

    WIP - everything

--- core/helpers/platforms/pi/qrcode.py
+++ core/helpers/platforms/pi/qrcode.py
@@ -4,4 +4,4 @@ from helper.QrCodeHelper import str_to_qr_code as parent_str_to_qr_code
 
 class PiQrCodeHelper(BaseQrCodeHelper):
     def str_to_qr_code(self, value):
-        parent_str_to_qr_code(value)
+        return parent_str_to_qr_code(value)
