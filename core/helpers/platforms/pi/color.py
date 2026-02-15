1ab014a389cf1e3c89bb1640a23df2c1e3ef55e6
Felix Holfelder <felixholfelder1@t-online.de>
2026-02-15 12:42:25 +0100

    WIP - everything

--- core/helpers/platforms/pi/color.py
+++ core/helpers/platforms/pi/color.py
@@ -1,9 +1,10 @@
 from core.helpers.base.color import BaseColorHelper
+from helper.ColorHelper import ColorHelper
 
 
 class PiColorHelper(BaseColorHelper):
     def __init__(self):
-        self.helper = PiColorHelper()
+        self.helper = ColorHelper()
 
     def to_rgb(self, hex_value):
         return self.helper.to_rgb(hex_value)
