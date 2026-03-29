27c5f1f0e15a8d5a0526f46aba994ee777c5bb37
Felix Holfelder <felixholfelder1@t-online.de>
2026-03-29 18:20:51 +0200

    cleaned factory method for strip settings

--- core/settings/base/strip.py
+++ core/settings/base/strip.py
@@ -8,5 +8,21 @@ class BaseStripSettings:
     def get_curr_brightness(self) -> float:
         raise NotImplementedError
 
-    def update_settings(self, is_active, brightness, length):
+    def is_static_color(self) -> bool:
+        raise NotImplementedError
+
+    def get_static_color(self) -> str:
+        raise NotImplementedError
+
+    def get_strip_settings(self):
+        raise NotImplementedError
+
+    def update_settings(
+        self,
+        is_active: bool | None = None,
+        brightness: float | None = None,
+        length: int | None = None,
+        is_static_color: bool | None = None,
+        static_color: str | None = None,
+    ):
         raise NotImplementedError
--- core/settings/pi/strip.py
+++ core/settings/pi/strip.py
@@ -1,25 +1,88 @@
+import json
+import re
+
+from core.factories.helper_factories import create_settings_sync_helper
 from core.settings.base.strip import BaseStripSettings
-from helper.strip_settings_helper import StripSettingsHelper
+from helper.constants import Constants
 
 
-class PiStripSettings(BaseStripSettings):
+class StripSettingsHelper(BaseStripSettings):
+    SETTING = "strip-settings.json"
+    STRIP_SETTINGS_PATH = f"{Constants.settings_path()}/{SETTING}"
+
     def __init__(self):
-        self.settings = StripSettingsHelper()
+        self.settings_sync_helper = create_settings_sync_helper()
 
     def is_strip_active(self) -> bool:
-        return self.settings.is_strip_active()
+        settings = self.get_strip_settings()
+        return settings["isStripEnabled"]
 
     def get_led_length(self) -> int:
-        return self.settings.get_led_length()
+        settings = self.get_strip_settings()
+        return settings["amountLeds"]
 
     def get_curr_brightness(self) -> float:
-        return self.settings.get_curr_brightness()
+        settings = self.get_strip_settings()
+        return settings["brightness"]
 
     def is_static_color(self) -> bool:
-        return self.settings.is_static_color()
+        settings = self.get_strip_settings()
+        return settings["isStaticColor"]
 
     def get_static_color(self) -> str:
-        return self.settings.get_static_color()
+        settings = self.get_strip_settings()
+        return settings["staticColor"].upper()
 
-    def update_settings(self, is_active, brightness, length):
-        self.settings.update_settings(is_active, brightness, length)
+    def get_strip_settings(self):
+        def _get_data():
+            with open(self.STRIP_SETTINGS_PATH) as file:
+                data = json.load(file)
+                return data
+
+        try:
+            return _get_data()
+        except Exception:
+            self.settings_sync_helper.reset_settings_file(self.SETTING)
+            return _get_data()
+
+    def update_settings(
+        self,
+        is_active: bool | None = None,
+        brightness: float | None = None,
+        length: int | None = None,
+        is_static_color: bool | None = None,
+        static_color: str | None = None,
+    ):
+        _is_active = (
+            is_active if is_active is not None else self.is_strip_active()
+        )
+        _brightness = (
+            brightness if brightness is not None else self.get_curr_brightness()
+        )
+        _length = length if length is not None else self.get_led_length()
+        _is_static_color = (
+            is_static_color
+            if is_static_color is not None
+            else self.is_static_color()
+        )
+
+        regex = "^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$"
+        p = re.compile(regex)
+        if static_color is None:
+            _static_color = self.get_static_color()
+        elif re.search(p, static_color):
+            _static_color = static_color
+        else:
+            _static_color = self.get_static_color()
+
+        data = self.get_strip_settings()
+        data["isStripEnabled"] = _is_active
+        data["brightness"] = _brightness
+        data["amountLeds"] = _length
+        data["isStaticColor"] = _is_static_color
+        data["staticColor"] = _static_color
+
+        with open(self.STRIP_SETTINGS_PATH, "r+") as file:
+            file.seek(0)
+            json.dump(data, file, indent=4)
+            file.truncate()
